c73d7a97522741b43479c7fddb0257230abe8749
Matthias Puech <puech@cs.mcgill.ca>
2015-09-05 16:09:33 +0200

    Processor: refactored reset in sync mode

--- processor.cc
+++ processor.cc
@@ -79,9 +79,11 @@ void Processor::SetFrequency(int8_t lfo_no) {
   if (reset_triggered_[lfo_no]) {
     if (ui_->sync_mode()) {
       lfo_[lfo_no].set_period(last_reset_[lfo_no]);
+      lfo_[lfo_no].Reset(reset_subsample_[lfo_no]);
       synced_[lfo_no] = true;
+    } else {
+      lfo_[lfo_no].Reset(reset_subsample_[lfo_no]);
     }
-    lfo_[lfo_no].Reset(reset_subsample_[lfo_no]);
     reset_trigger_armed_[lfo_no] = false;
     last_reset_[lfo_no] = 0;
   } else {
