f115a45d537f62046fb1ddbbad16c042248aedc3
Matthias Puech <puech@cs.mcgill.ca>
2015-11-13 18:22:41 +0100

    Flasher: finish up the flasher's UI

--- flasher/flasher.py
+++ flasher/flasher.py
@@ -41,7 +41,7 @@ from stm32loader import *
 
 import serial
 
-firmware_name = "batumi-0.4"
+firmware_name = "batumi-0.41"
 
 def serial_ports():
     """ Lists serial port names
@@ -82,34 +82,37 @@ def center(toplevel):
 
 class Application(Frame):
 
-    def confirm_flash(self):
-        result = tkMessageBox.askquestion("Flash", "Are You Sure?", icon='warning')
-        return result == 'yes'
-
     def flash(self):
         cmd = CommandInterface()
-        self.status.set("opening port "+self.port+"...")
+        self.status.set("Opening port \""+self.port+"\"...")
+        root.update()
         try:
             cmd.open(self.port, 115200)
-            print("Open port %(port)s succeeded" % {'port':self.port})
             self.status.set("Initializing chip...")
+            root.update()
             try:
                 cmd.initChip()
             except:
-                raise CmdException("Can't find the chip. Please check jumpers position.")
+                raise CmdException("Can't find the chip. Please check jumper position.")
             bootversion = cmd.cmdGet()
-            print("Bootloader version %X" % bootversion)
             id = cmd.cmdGetID()
-            print("Chip id: 0x%x (%s)" % (id, chip_ids.get(id, "Unknown")))
 
-            if (self.confirm_flash()):
-                self.status.set("erasing memory...")
-                cmd.cmdEraseMemory()
-                self.status.set("writing firmware...")
-                cmd.writeMemory(0x08000000, self.data)
-                self.status.set("Done.")
+            self.status.set("Erasing memory...")
+            root.update()
+            cmd.cmdEraseMemory()
+            self.status.set("Writing firmware (may take a minute)...")
+            root.update()
+            cmd.writeMemory(0x08000000, self.data)
+            self.status.set("Done.")
+            self.done = 42;
+            self.ok['state'] = 'normal'
+            self.ok['text'] = 'Quit'
+            self.instruct.set("Now you can put the jumper back in its original position, plug Batumi back in your case and have fun!")
+            root.update()
+
         except CmdException as e:
             self.status.set("Error: "+str(e))
+            self.start()
         finally:
             cmd.releaseChip()
 
@@ -117,25 +120,39 @@ class Application(Frame):
         if not(hasattr(self, 'ports')):
             self.status.set("listing available devices...")
             self.ports = serial_ports()
-            self.status.set("ready.")
-            self.instruct.set("Now plug Batumi to this computer via USB and power it up. Make sure to set the jumpers on the back as indicated in the manual.")
+            self.status.set("Ready.")
+            self.instruct.set("Now plug Batumi to this computer via USB. It should not be connected to the Eurorack power. Make sure to set the jumper on the back to the Update position (left).")
         elif not(hasattr(self, 'port')):
-            self.status.set("detecting new devices...")
+            self.status.set("Detecting new devices...")
             newports = serial_ports()
             l = list(set(newports) - set(self.ports))
             if len(l) < 1:
                 self.status.set("Error: No new device detected.")
+                self.start()
                 return
             elif len(l) > 1:
                 self.status.set("Several new devices detected. Please connect one at a time.")
+                self.start()
                 return
 
-            port = l[0]
-            self.status.set("found new device at port \""+port+"\".")
-            self.instruct.set("Are you really sure you want to flash Batumi with firmware \""^
-                              firmware_name^"\"?")
-        else:
+            self.port = l[0]
+            self.status.set("found new device at port \""+self.port+"\".")
+            self.instruct.set("Are you really sure you want to flash Batumi with firmware \""+firmware_name+"\"?")
+        elif (not (hasattr(self, 'done'))):
+            self.instruct.set("")
+            self.ok['state'] = 'disabled'
+            root.update()
             self.flash()
+
+        else:
+            root.destroy()
+
+    def start(self):
+        if (hasattr(self, 'ports')): delattr(self, 'ports')
+        if (hasattr(self, 'port')): delattr(self, 'port')
+        if (hasattr(self, 'done')): delattr(self, 'done')
+        self.ok['state'] = 'normal'
+        self.instruct.set("Make sure Batumi is unplugged and click Ok.")
 
     def createWidgets(self):
 
@@ -153,13 +170,12 @@ class Application(Frame):
               font=("Courier", 16)).pack()
 
         self.instruct = StringVar()
-        self.instruct.set("Make sure Batumi is unplugged and click Ok.")
 
         Label(self,
               textvariable=self.instruct,
               anchor=W,
               justify=LEFT,
-              height=3,
+              height=4,
               wraplength=300).pack()
 
         self.ok = Button(self, text="Ok", command=self.ok, font=("Helvetica", 14))
@@ -173,6 +189,8 @@ class Application(Frame):
               width=300,
               relief=SUNKEN,
               anchor=W).pack(fill=X)
+
+        self.start()
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
